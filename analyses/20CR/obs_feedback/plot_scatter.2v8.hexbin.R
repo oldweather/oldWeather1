58dd848850d85e6295c78f6d864331d16cf0b75a
Philip Brohan <philip@brohan.org>
2014-01-21 17:34:01 +0000

    Efficiency and cosmetic improvements.

--- analyses/20CR/obs_feedback/plot_scatter.2v8.hexbin.R
+++ analyses/20CR/obs_feedback/plot_scatter.2v8.hexbin.R
@@ -11,8 +11,8 @@ Month<-1
 Day<-1
 Hour<-0
 
-obs.2<-NULL
-obs.8<-NULL
+obs.2<-list()
+obs.8<-list()
 
 c.date<-chron(dates=sprintf("%04d/%02d/%02d",Year,Month,Day),
           times=sprintf("%02d:00:00",Hour),
@@ -26,19 +26,21 @@ read.obs.file<-function(n.count) {
    Day<-days(n.date)
    
    for(Hour in c(0,6,12,18)) {
- 
+
+      idx<-n.count*4+as.integer(Hour/6)+1
       o.2<-TWCR.get.obs.1file(Year,Month,Day,Hour,version='3.3.2')
       o.2$oW.obs<-grepl('9931',o.2$UID) # Find the oW obs
-      if(is.null(obs.2)) { obs.2<<-o.2
-      } else obs.2<<-rbind(obs.2,o.2)
+      obs.2[[idx]]<<-o.2
       o.8<-TWCR.get.obs.1file(Year,Month,Day,Hour,version='3.3.8')
       o.8$oW.obs<-grepl('9931',o.8$UID) # Find the oW obs
-      if(is.null(obs.8)) { obs.8<<-o.8
-      } else obs.8<<-rbind(obs.8,o.8)
+      obs.8[[idx]]<<-o.8
 
     }
  }
 f<-lapply(n.count,read.obs.file)
+# Pack all the individual data frames into one big one
+obs.2<-do.call('rbind',obs.2)
+obs.8<-do.call('rbind',obs.8)
 
 # Get rid of the outliers
 range<-c(0,3)
@@ -47,14 +49,14 @@ is.na(obs.8$Analysis.pressure.spread[w])<-T
 w<-which(obs.2$Analysis.pressure.spread>range[2])
 is.na(obs.2$Analysis.pressure.spread[w])<-T
 
-png('tst.png',width=1200,height=600)
-trellis.par.set('fontsize',list('text'=18))
+pdf('332_v_338.pdf',width=11,height=7)
+trellis.par.set('fontsize',list('text'=16))
 
 gp_blue  = gpar(col=rgb(0,0,1,1),fill=rgb(0,0,1,1),lwd=2)
 gp_red  = gpar(col=rgb(1,0,0,1),fill=rgb(1,0,0,1),lwd=2)
-gp_grey  = gpar(col=rgb(0.8,0.8,0.8,1),fill=rgb(0.8,0.8,0.8,1))
+gp_grey  = gpar(col=rgb(0.2,0.2,0.2,1),fill=rgb(0.2,0.2,0.2,1))
 
-pushViewport(viewport(width=0.45,height=0.9,x=0.05,y=0.05,
+pushViewport(viewport(width=0.5,height=1.0,x=0.0,y=0.0,
                           just=c("left","bottom"),name="vp_main"))
 
 
@@ -62,7 +64,7 @@ l<-lm(obs.2$Analysis.pressure.spread[-which(obs.2$oW.obs)]~obs.8$Analysis.pressu
 print(hexbinplot(obs.2$Analysis.pressure.spread[-which(obs.2$oW.obs)]~obs.8$Analysis.pressure.spread[-which(obs.8$oW.obs)],
            aspect=1,
            main='Original obs',
-           xlab = "Spread in 3.3.8", ylab = "Spread in 3.3.3",
+           xlab = "Spread in 3.3.8", ylab = "Spread in 3.3.2",
            xlim=range,ylim=range,
            panel = function(...) {
              panel.hexbinplot(...)
@@ -75,7 +77,7 @@ print(hexbinplot(obs.2$Analysis.pressure.spread[-which(obs.2$oW.obs)]~obs.8$Anal
              }),newpage=F)
 
 upViewport(0)
-pushViewport(viewport(width=0.45,height=0.9,x=0.5,y=0.05,
+pushViewport(viewport(width=0.5,height=1.0,x=0.5,y=0.00,
                           just=c("left","bottom"),name="vp_main"))
 
 
@@ -83,7 +85,7 @@ l<-lm(obs.2$Analysis.pressure.spread[which(obs.2$oW.obs)]~obs.8$Analysis.pressur
 print(hexbinplot(obs.2$Analysis.pressure.spread[which(obs.2$oW.obs)]~obs.8$Analysis.pressure.spread[which(obs.8$oW.obs)],
            aspect=1,
            main='oldWeather obs',
-           xlab = "Spread in 3.3.8", ylab = "Spread in 3.3.3",
+           xlab = "Spread in 3.3.8", ylab = "Spread in 3.3.2",
            xlim=range,ylim=range,
            panel = function(...) {
              panel.hexbinplot(...)
