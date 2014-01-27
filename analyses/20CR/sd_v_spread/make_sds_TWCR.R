a924b63445f7fa94a09eec2f771357cb4ab49109
Philip Brohan <philip@brohan.org>
2014-01-27 13:23:07 +0000

    Added study od standard deviations.

---
+++ analyses/20CR/sd_v_spread/make_sds_TWCR.R
@@ -0,0 +1,51 @@
+# Make standard deviations 1981-2010 from 20CR.
+#  Store the results as Rdata files
+
+library(GSDF.TWCR)
+library(chron)
+library(multicore)
+
+base.dir<-'/data/cr2/hadpb/20CR/version_3.2.1/hourly/standard.deviations'
+variable<-'prmsl'
+base.dir<-sprintf("%s/%s",base.dir,variable)
+dir.create(base.dir)
+
+c.date<-chron(dates="1981/01/01",
+          times="00:00:00",
+          format=c(dates='y/m/d',times='h:m:s'))
+
+n.count<-seq(1,365*24)
+
+make.sd<-function(n.count) {
+  
+   n.date<-c.date+n.count-1
+   Month<-months(n.date)
+   Day<-days(n.date)
+
+   for(Hour in c(0,6,12,18)) {
+     
+     f.name<-sprintf("%s/sd.%02d.%02d.%02d.rdata",
+                    base.dir,Month,Day,Hour)
+     if(file.exists(f.name)) return()
+     
+     Accumulator<-array(dim=c(30,16380))
+
+      for(Year in seq(1981,2010)) {
+
+         p<-TWCR.get.slice.at.hour(variable,Year,Month,Day,Hour)
+         Accumulator[Year-1980,]<-as.vector(p$data)
+
+      }
+
+      s<-sd(Accumulator)
+
+      twcr.sd<-TWCR.get.slice.at.hour(variable,1981,Month,Day,Hour)
+      twcr.sd$data[]<-s
+
+      save(twcr.sd,file=f.name,compress=TRUE)
+
+    }
+    gc(verbose=F)
+ }
+     
+mclapply(n.count,make.sd,mc.cores=3,mc.preschedule=FALSE)
---
+++ analyses/20CR/sd_v_spread/sd_from_TWCR.R
@@ -0,0 +1,27 @@
+# Calculate the weather standard deviation 1981-2010 from 20CR.
+#  Just for one time in the seasonal cycle.
+
+library(GSDF.TWCR)
+
+variable<-'prmsl'
+
+Month<-3
+Day<-12
+Hour<-6
+
+Accumulator<-array(dim=c(30,16380))
+
+for(Year in seq(1981,2010)) {
+
+   p<-TWCR.get.slice.at.hour(variable,Year,Month,Day,Hour)
+   Accumulator[Year-1980,]<-as.vector(p$data)
+
+ }
+
+ s<-sd(Accumulator)
+
+ twcr.sd<-TWCR.get.slice.at.hour(variable,1981,Month,Day,Hour)
+ twcr.sd$data[]<-s
+
+ name<-sprintf("TWCR.%s.sd.%02d.%02d.%02d.rdata",variable.Month,Day,Hour)
+ save(twcr.sd,file=name)
